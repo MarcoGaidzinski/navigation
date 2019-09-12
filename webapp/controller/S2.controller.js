a5dffce605b4b35d6d91640bc7cdf82a1091fe62
Marco Aurélio Gaidzinski <gaidzinski@gmail.com>
2019-09-12 18:05:55 +0000

    Navigation versão 01

---
+++ webapp/controller/S2.controller.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("ovly.navigation.controller.S2", {
+
+		onInit: function () {
+
+		},
+
+		onBack: function(oEvent){
+			this.getOwnerComponent().getRouter().navTo("inicial");
+		},
+		
+		onPress: function(oEvent){
+			
+		}
+	});
+
+});
